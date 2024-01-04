b95806adac714ab66e165354a89ad42a34e6f98b
Tony <santiago.montesb@gmail.com>
2024-01-03 23:15:26 -0500

    projects slider ready

--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -1,17 +1,20 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import ProjectCard from './ProjectCard';
-import Swiper from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination } from 'swiper/modules';
 import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
 const projects = [
   {
-    id: 'ecn1',
+    id: 'modal',
     title: 'Song Preloading',
     scenario: 'Scenario 1',
     stars: 5,
     description:
       'When a song is played in the app, the music player takes the initiative to preload not only the current second of the song but also a few seconds ahead of it ...',
-    imageUrl: 'https://picsum.photos/100?image=101',
+    imageUrl: 'https://picsum.photos/900?image=101',
   },
   {
     id: 'ecn2',
@@ -20,82 +23,74 @@ const projects = [
     stars: 5,
     description:
       'Blackhole allows users to proactively download songs when a stable internet connection is available. By downloading songs in advance, users can build their offline music library ...',
-    imageUrl: 'https://picsum.photos/100?image=103',
+    imageUrl: 'https://picsum.photos/900?image=103',
   },
   {
     id: 'ecn3',
-    title: 'Offline Downloaded Reproduction',
+    title: 'Offline Reproduction',
     scenario: 'Scenario 3',
     stars: 5,
     description:
       'The enables users to access and enjoy their recently played songs even when they have no internet connectivity. By preserving the history of previously played songs you can continue to enjoy your songs even when ...',
-    imageUrl: 'https://picsum.photos/100?image=104',
+    imageUrl: 'https://picsum.photos/900?image=104',
   },
   {
     id: 'ecn4',
-    title: 'No-Connectivity Features',
+    title: 'No-Connectivity Ft.',
     scenario: 'Scenario 4',
     stars: 5,
     description:
-      'In various pages, such as “Last session”, “Favorites” and “Stats” the app offers features that remain accessible even when there is no internet connectivity, it also occurs with some features ...',
-    imageUrl: 'https://picsum.photos/100?image=107',
+      'In various pages, such as “Last session”, “Favorites” and “Stats” the app offers features that remain accessible even when there is no internet connectivity, it ',
+    imageUrl: 'https://picsum.photos/900?image=107',
   },
   {
     id: 'ecn5',
-    title: 'Offline Playlist Management',
+    title: 'Offline Playlist',
     scenario: 'Scenario 5',
     stars: 5,
     description:
       'In this scenario, the app misleadingly appears to allow users to create new playlists and add songs to them when no connectivity is available. But the playlists remain empty ...',
-    imageUrl: 'https://picsum.photos/100?image=109',
+    imageUrl: 'https://picsum.photos/900?image=109',
   },
 ];
 
 const Projects = () => {
-    useEffect(() => {
-        const projectsSlider = new Swiper('.projects-slider', {
-          speed: 600,
-          loop: true,
-          slidesPerView: 'auto',
-          breakpoints: {
-            320: {
-              slidesPerView: 1,
-              spaceBetween: 40,
-            },
-            1200: {
-              slidesPerView: 3,
-            },
-          },
-          pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
-          },
-        });
-      
-        const interval = setInterval(() => {
-          projectsSlider.slideNext();
-        }, 5000); // Slide every 5 seconds
-      
-        // Cleanup the interval when the component is unmounted
-        return () => {
-          clearInterval(interval);
-          projectsSlider.destroy();
-        };
-      }, []);
-      
+  const [isPC, setIsPC] = useState(window.innerWidth >= 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsPC(window.innerWidth >= 768);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <section id="projects" className="projects">
-        <h2 className="heading-secondary">Projects</h2>
-        <div className="container">
-            <div className="projects-slider swiper">
-            <div className="swiper-wrapper">
-                {projects.map((project) => (
-                <ProjectCard key={project.id} project={project} />
-            ))}
-            </div>
-            <div className="swiper-pagination"></div>
-            </div>
-        </div>
+      <h2 className="heading-secondary">Projects</h2>
+      <div className="container">
+        <Swiper
+          modules={[Navigation, Pagination]}
+          className="projects-slider"
+          spaceBetween={40}
+          slidesPerView={isPC ? 3 : 1}
+          centeredSlides={true}
+          loop={true}
+          pagination={{ clickable: true }}
+          //onSwiper={(swiper) => console.log(swiper)}
+          //onSlideChange={() => console.log('slide change')}
+        >
+          {projects.map((project) => (
+            <SwiperSlide key={project.id}>
+              <ProjectCard project={project} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
     </section>
   );
 };
