93c8495956ea0a138006d88fe6ec8b1ab28c4484
Tony <santiago.montesb@gmail.com>
2024-08-18 15:33:42 -0500

    update description & about-me

--- src/AboutMe/AboutMe.js
+++ src/AboutMe/AboutMe.js
@@ -6,35 +6,19 @@ function AboutMe(props) {
 			<div className="u-text-center">
 				<h2 className="heading-secondary">About Me</h2>
 				<p className="intro-text">
-					I am a passionate Systems Engineering and Electronic Engineering student 
+					I am a passionate Systems Engineering and Electronic Engineering last-year student 
 					interested in developing creative and innovative solutions. 
-					As a freelance developer at&nbsp;
-					<a target="_blank" href="https://somositti.com/" rel="noreferrer">ITTI</a>, 
-					&nbsp;I worked in both backend and frontend roles, primarily in JavaScript, along with 
-					cloud functions. I have also worked as a tutor, monitor, and research assistant 
-					in the design of various undergraduate and graduate courses, including master's 
-					degree programs; mainly focused on software engineering, helping more than 200 
-					students achieve their academic goals.
 					<br />
-					<br />
-					I am looking for opportunities to enhance my skills in the field of AI and to 
-					work in this field in the future. I enjoy applying the knowledge acquired to 
-					practical solutions and am constantly seeking opportunities to learn about 
-					new topics. Also I am passionate about teaching and helping others with the subjects 
-					in which I excel, as 
+					My work in Artificial Intelligence includes my recent publication, 
+					<a target="_blank" href="https://aclanthology.org/2024.lchange-1.4/" rel="noreferrer">Historical Ink: SSD</a>, 
+					and my role as an ML Intern at Cornell. I am passionate about contributing to AI-related projects, whether in research or industry, 
+					where I can apply my expertise to drive innovation.
+					I continually seek opportunities to expand my knowledge and enjoy teaching and helping others in areas where I excel, as I believe that
 					<br />
 					<span className="about__phrase">I believe knowledge is the most solid foundation for progress</span>. 
 					<br />
-					My goal is to continue growing professionally and adding value to challenging 
-					and creative projects in the field of software and AI development.
 					<br />
-					<br />
-					Along with my colleagues, I founded the&nbsp;
-					<a target="_blank" href="https://github.com/Open-Source-Uniandes" rel="noreferrer">Open Source</a>
-					&nbsp;leadership at Universidad de los Andes to encourage and support the creation of 
-					tools for the university community made by the community itself. As co-leader 
-					of the team, I am responsible for administrative management and providing proposed 
-					solutions to the challenges raised. 
+					My goal is to contribute to challenging and innovative projects in AI research, particularly in Computer Vision and Natural Language Processing.
 				</p>
 			</div>
 		</section>
